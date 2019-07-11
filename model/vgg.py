fe82b3a57218aaf1564e67f4fdbc5a14df97a971
Sung Man Cho <pccho9108@gmail.com>
2019-07-11 22:34:28 +0900

    add backbone_vgg

--- model/vgg.py
+++ model/vgg.py
@@ -35,3 +35,48 @@ class VGG(nn.Module):
         
         # l2 normalization
         x = torch.div(x, torch.norm(x, 2, axis=1, True))
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.PReLU(v)]
+            else:
+                layers += [conv2d, nn.PReLU(v)]
+            
+            in_channels = v
+
+    return nn.Sequential(*layers)
+
+def vgg16_bn(**kwargs):
+    model = VGG(make_layers(cfg['VGG16_bn'], batch_norm=True), **kwargs)
+    return model
+    
+def vgg19_bn(**kwargs):
+    model = VGG(make_layers(cfg['VGG19_bn'], batch_norm=True), **kwargs)
+    return model
+
