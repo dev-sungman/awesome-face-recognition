03247c04b47fe1fd459b894c19888d60a350a95b
Sung Man Cho <pccho9108@gmail.com>
2019-07-21 14:12:05 +0900

    update train.py

--- train.py
+++ train.py
@@ -1,1 +1,21 @@
+import torch 
+import torch.nn as nn
+
+import sys
+import os
+import argparse
+
+def parse_arguments(argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+
+    return parser.parse_args(argv)
+
+def main(args):
+
+
+if __name__ == '__main__':
+    parse_arguments(sys.argv[1:])
+
 
