efe2494f896b1b43034c6cf846e4e9661c127fb2
Sung Man Cho <pccho9108@gmail.com>
2019-10-21 21:49:28 +0900

    update : find diagonal element

--- model/regularface.py
+++ model/regularface.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+import numpy as np
 
 class RegularFace(nn.Module):
     def __init__(self, in_features, out_features):
@@ -13,21 +15,24 @@ class RegularFace(nn.Module):
     def forward(self, x):
         weight_norm = F.normalize(self.weight, p=2, dim=1)
         cos = torch.mm(weight_norm, weight_norm.t())
-        print(cos) 
+        
         # for numerical stability
         cos.clamp(-1, 1)
         
         # for eliminate element w_i = w_j
-        cos1 = cos.detach()
-        cos1.scatter_(1, torch.arange(self.out_features).view(-1, 1).long(), -100)
-        
-        _,indices = torch.max(cos1, dim=0)
+        cos_ind = cos.detach()
+
+        ind = np.diag_indices(cos.shape[0])
+        min_ind = torch.min(cos_ind) - 1
+        cos_ind[ind[0], ind[1]] = torch.full((cos_ind.shape[0],), min_ind)
+
+        _,indices = torch.max(cos_ind, dim=0)
         mask = torch.zeros((self.out_features, self.out_features))
-        print(indices.view(-1,1).long(), indices.view(-1,1).shape)
         mask.scatter_(1, indices.view(-1,1).long(), 1)
         
         exclusive_loss = torch.dot(cos.view(cos.numel()), mask.view(mask.numel())) / self.out_features
-
+        
+        return exclusive_loss
 
 if __name__ == '__main__':
     x = torch.randn([512, 10486])
